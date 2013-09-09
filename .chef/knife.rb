0d5c0447e8a424735b5b573f8c32f22402931a42
tangerinedream <gary.mail.low@gmail.com>
2013-09-09 10:39:55 -0500

    Added knife.rb

---
+++ .chef/knife.rb
@@ -0,0 +1,15 @@
+# See http://docs.opscode.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "tdreamcheftraining"
+client_key               "#{current_dir}/tdreamcheftraining.pem"
+validation_client_name   "tdreamcheftraining-validator"
+validation_key           "#{current_dir}/tdreamcheftraining-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/tdreamcheftraining"
+cache_type               'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
+#GHS
+knife[:editor] = "/usr/bin/vi"
